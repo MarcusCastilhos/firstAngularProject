5b69bdd7a8ffaecf9b7eb629fa56d1f7a5b51ba1
MarcusCastilhos <marcus.drummer@live.com>
2024-05-23 19:58:38 -0300

    feat: clear default template and reset style.css

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'firstAngularProject' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('firstAngularProject');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
