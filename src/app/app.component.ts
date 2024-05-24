6f19dc13dec2c54e6c4916db4fa699e7c431125a
MarcusCastilhos <marcus.drummer@live.com>
2024-05-24 20:07:01 -0300

    edição

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { LivrosService } from './services/livros.service';
 import { LivrosTabelaComponent } from './components/livros-tabela/livros-tabela.component';
+import { TituloComponent } from './components/titulo/titulo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [LivrosTabelaComponent],
+  imports: [LivrosTabelaComponent, TituloComponent],
   providers: [LivrosService]
 })
 export class AppComponent {
