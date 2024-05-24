c3e0408bf60446ac9b9074d559d77a5857398ae1
MarcusCastilhos <marcus.drummer@live.com>
2024-05-24 11:46:19 -0300

    fix: correção no app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 import { LivrosService } from './services/livros.service';
 import { LivrosTabelaComponent } from './components/livros-tabela/livros-tabela.component';
 
-interface Livro {
-  id: number;
-  title: string;
-}
-
 @Component({
   selector: 'app-root',
   standalone: true,
