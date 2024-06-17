37d7c55db0b5f63bc4366fc47ca059587c7b5c95
MarcusCastilhos <marcus.drummer@live.com>
2024-06-17 12:52:08 -0300

    feat: add button component "add-book".

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { LivrosService } from './services/livros.service';
 import { LivrosTabelaComponent } from './components/livros-tabela/livros-tabela.component';
 import { TituloComponent } from './components/titulo/titulo.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [LivrosTabelaComponent, TituloComponent],
-  providers: [LivrosService]
+  imports: [LivrosTabelaComponent, TituloComponent, AddBookComponent],
+  providers: [LivrosService],
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
