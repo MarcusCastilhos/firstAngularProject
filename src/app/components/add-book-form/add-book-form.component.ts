b081cfd4d95860fa8cf9c7950cd10d9c080139e1
MarcusCastilhos <marcus.drummer@live.com>
2024-07-10 11:08:28 -0300

    feat: added form to add-book-form html and ts.

--- src/app/components/add-book-form/add-book-form.component.ts
+++ src/app/components/add-book-form/add-book-form.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-book-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-book-form.component.html',
-  styleUrl: './add-book-form.component.scss'
+  styleUrl: './add-book-form.component.scss',
 })
 export class AddBookFormComponent {
+  bookForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.bookForm = this.fb.group({
+      title: ['', Validators.required],
+      year: ['', [Validators.required, Validators.pattern(/^\d{4}$/)]],
+      author: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.bookForm.valid) {
+      console.log(this.bookForm.value);
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 }
