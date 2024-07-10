1197492bdf0e48feefdb74241c8b8ade5c2bc8ef
MarcusCastilhos <marcus.drummer@live.com>
2024-07-10 10:38:02 -0300

    feat: added eventemitter and onaddbook function

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-book',
   standalone: true,
   imports: [],
   templateUrl: './add-book.component.html',
-  styleUrl: './add-book.component.scss'
+  styleUrl: './add-book.component.scss',
 })
 export class AddBookComponent {
+  @Output() addBook = new EventEmitter<void>();
 
+  onAddBook() {
+    this.addBook.emit();
+  }
 }
