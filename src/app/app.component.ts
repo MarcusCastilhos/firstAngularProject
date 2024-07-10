5b5a29ef93d0ce7fceebc4a83199d9c2066e1a25
MarcusCastilhos <marcus.drummer@live.com>
2024-07-10 10:11:49 -0300

    feat: update code to receive component 'add-book-form'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,19 @@ import { LivrosService } from './services/livros.service';
 import { LivrosTabelaComponent } from './components/livros-tabela/livros-tabela.component';
 import { TituloComponent } from './components/titulo/titulo.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
+import { AddBookFormComponent } from './components/add-book-form/add-book-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [LivrosTabelaComponent, TituloComponent, AddBookComponent],
+  imports: [
+    LivrosTabelaComponent,
+    TituloComponent,
+    AddBookComponent,
+    AddBookFormComponent,
+  ],
   providers: [LivrosService],
 })
 export class AppComponent {}
